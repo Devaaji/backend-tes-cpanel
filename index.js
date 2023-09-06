55b853b305adc9f9ba98f69ebaddfa53206eb258
Devaaji <devaaji5@gmail.com>
2023-09-06 11:57:41 +0700

    Jadi ganteng

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Deva Aji Saputra");
+  res.send("Deva Aji Saputra GANTENGGG");
 });
 
 app.listen(3030, () => {
